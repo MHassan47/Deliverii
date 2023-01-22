5109c369ff56ad73afda7f3b570fddf4f1efbdb5
Mohamed <mhassankd35@gmail.com>
2023-01-22 13:09:13 -0600

    creates layout of restaurant page

--- screens/RestaurantScreen.tsx
+++ screens/RestaurantScreen.tsx
@@ -1,6 +1,11 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import {
+  ArrowLeftIcon,
+  QuestionMarkCircleIcon,
+  ChevronRightIcon,
+} from "react-native-heroicons/outline";
+import { StarIcon, MapPinIcon } from "react-native-heroicons/solid";
 import {
   useRoute,
   RouteProp,
@@ -50,6 +55,38 @@ const RestaurantScreen = () => {
         >
           <ArrowLeftIcon color="#00CCBB" size={20} />
         </TouchableOpacity>
+      </View>
+      <View>
+        <View className="bg-white">
+          <Text className="text-3xl">{params.title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color="green" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{params.rating}</Text> ·{" "}
+                {params.genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                Nearby · {params.genre}
+              </Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2 pb-4">
+            {params.shortDescription}
+          </Text>
+          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+            <QuestionMarkCircleIcon color="gray" />
+            <Text className="font-bold flex-1">Have a food allergy?</Text>
+            <ChevronRightIcon color="#00CCBB" />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+        </View>
       </View>
     </ScrollView>
   );
