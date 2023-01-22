0d2e59d64dddfc96ac0b864b2b2f69125951704f
Mohamed <mhassankd35@gmail.com>
2023-01-22 16:41:34 -0600

    creates item interface, remove item from basket reducer function

--- redux/features/basketSlice.ts
+++ redux/features/basketSlice.ts
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface BasketSlice {
-  items: string[];
+  items: Item[];
+}
+
+interface Item {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
 }
 
 const initialState: BasketSlice = {
@@ -15,7 +23,18 @@ export const basketSlice = createSlice({
     addToBasket: (state, action) => {
       state.items = [...state.items, action.payload];
     },
-    removeFromBasket: (state, action) => {},
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex(
+        (item: Item) => item.id === action.payload.id
+      );
+      let newBasket = [...state.items];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`No items left to remove!`);
+      }
+      state.items = newBasket;
+    },
   },
 });
 
@@ -23,4 +42,7 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 export const selectBasketItems = (state: any) => state.basket.items;
+
+export const selectBasketItemsWithId = (state: any, id: string) =>
+  state.basket.items.filter((item: Item) => item.id === id);
 export default basketSlice.reducer;
