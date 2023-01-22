dfc3f7a84e7dabfd10f8f9d9f9f06351398914a0
Mohamed <mhassankd35@gmail.com>
2023-01-22 12:52:43 -0600

    adds restaurant image and navigation back button

--- screens/RestaurantScreen.tsx
+++ screens/RestaurantScreen.tsx
@@ -1,7 +1,13 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
-
-import { useRoute, RouteProp, ParamListBase } from "@react-navigation/native";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import {
+  useRoute,
+  RouteProp,
+  ParamListBase,
+  useNavigation,
+} from "@react-navigation/native";
+import { urlFor } from "../sanity";
 
 interface props {
   id: string;
@@ -19,13 +25,32 @@ interface props {
 type ScreenRouteProp = RouteProp<ParamListBase, string> & {
   params: props;
 };
+
 const RestaurantScreen = () => {
   const route = useRoute<ScreenRouteProp>();
   const { params } = route;
+  const navigation = useNavigation();
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
     <ScrollView>
-      <Text>{params.title}</Text>
+      {/* <Text>{params.title}</Text> */}
+      <View className="relative">
+        <Image
+          source={{ uri: urlFor(params.imgUrl).url() }}
+          className="w-full h-56 bg-gray-300 p-4"
+        />
+        <TouchableOpacity
+          onPress={navigation.goBack}
+          className="absolute top-14 left-5 p-3 bg-gray-200 rounded-full"
+        >
+          <ArrowLeftIcon color="#00CCBB" size={20} />
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
