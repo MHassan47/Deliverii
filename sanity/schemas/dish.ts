d7abe4a9dd18a39751e1671a41b4530e2141bc52
Mohamed <mhassankd35@gmail.com>
2023-01-22 11:39:07 -0600

    small change to dish entity

--- sanity/schemas/dish.ts
+++ sanity/schemas/dish.ts
@@ -1,31 +1,31 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from "sanity";
 
 export default defineType({
-  name: 'dish',
-  title: 'Dish',
-  type: 'document',
+  name: "dish",
+  title: "Dish",
+  type: "document",
   fields: [
     defineField({
-      name: 'name',
-      title: 'Name of the Dish',
-      type: 'string',
+      name: "name",
+      title: "Name of the Dish",
+      type: "string",
       validation: (Rule) => Rule.required(),
     }),
     defineField({
-      name: 'short_description',
-      title: 'Short description',
-      type: 'string',
+      name: "short_description",
+      title: "Short description",
+      type: "string",
       validation: (Rule) => Rule.max(200),
     }),
     defineField({
-      name: 'image',
-      title: 'Image of the Dish',
-      type: 'image',
+      name: "image",
+      title: "Image of the Dish",
+      type: "image",
     }),
     defineField({
-      name: 'price',
-      title: 'Price of the Dish',
-      type: 'number',
+      name: "price",
+      title: "Price of the Dish",
+      type: "number",
     }),
   ],
-})
+});
