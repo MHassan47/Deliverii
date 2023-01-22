a70dfdea624c340f00e3891b851ca16cb3a85a14
Mohamed <mhassankd35@gmail.com>
2023-01-22 16:22:01 -0600

    creates add to basket item redux state  handler

--- components/DishRow.tsx
+++ components/DishRow.tsx
@@ -1,7 +1,9 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { urlFor } from "../sanity";
-
+import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+import { useDispatch, useSelector } from "react-redux";
+import { addToBasket, selectBasketItems } from "../redux/features/basketSlice";
 interface props {
   key: string;
   id: string;
@@ -12,28 +14,55 @@ interface props {
 }
 
 const DishRow: FC<props> = ({ id, name, description, price, image }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const dispatch = useDispatch();
+  const items = useSelector(selectBasketItems);
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+
   return (
-    <TouchableOpacity className="bg-white border p-4 border-gray-200 ">
-      <View className="flex-row">
-        <View className="flex-1 pr-2">
-          <Text className="text-lg mb-1">{name}</Text>
-          <Text className="text-gray-400">{description}</Text>
-          <Text className="text-gray-400">
-            ${Math.round(price * 100) / 100}
-          </Text>
+    <>
+      <TouchableOpacity
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        }`}
+        onPress={() => setIsPressed(!isPressed)}
+      >
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400">
+              ${Math.round(price * 100) / 100}
+            </Text>
+          </View>
+          <View>
+            <Image
+              style={{
+                borderWidth: 1,
+                borderColor: "#F3F3F4",
+              }}
+              source={{ uri: urlFor(image).url() }}
+              className="w-20 h-20 bg-gray-300 p-4"
+            />
+          </View>
         </View>
-        <View>
-          <Image
-            style={{
-              borderWidth: 1,
-              borderColor: "#F3F3F4",
-            }}
-            source={{ uri: urlFor(image).url() }}
-            className="w-20 h-20 bg-gray-300 p-4"
-          />
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity>
+              <MinusCircleIcon size={40} color="#00CCBB" />
+            </TouchableOpacity>
+            <Text>{items.length}</Text>
+            <TouchableOpacity onPress={addItemToBasket}>
+              <PlusCircleIcon size={40} color="#00CCBB" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      )}
+    </>
   );
 };
 
