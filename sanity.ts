247183509964abd3aacaafb1ffe97cad563e0de9
Mohamed <mhassankd35@gmail.com>
2023-01-21 19:44:46 -0600

    creates sanity client connection

---
+++ sanity.ts
@@ -0,0 +1,12 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+const client = sanityClient({
+  projectId: "g7tghljk",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2021-10-21",
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source: string) => builder.image(source);
