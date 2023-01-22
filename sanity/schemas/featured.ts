3055e2d6ca7aadb9fadd5340ca7deeaf7c76537a
Mohamed <mhassankd35@gmail.com>
2023-01-21 19:45:55 -0600

    creates featured entity table in santiy

---
+++ sanity/schemas/featured.ts
@@ -0,0 +1,27 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name of the Dish',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'short_description',
+      title: 'Short description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    }),
+    defineField({
+      name: 'restaurants',
+      type: 'array',
+      title: 'Restaurants',
+      of: [{type: 'reference', to: [{type: 'restaurant'}]}],
+    }),
+  ],
+})
