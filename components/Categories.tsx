71ea3b5a3a7968309fe821a63d90639d99caa7fb
Mohamed <mhassankd35@gmail.com>
2023-01-22 12:53:36 -0600

    refactors categories component to dynamically render from Sanity API request

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,20 +1,45 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CategoriesCard from "./CategoriesCard";
+import client, { urlFor } from "../sanity";
+
+interface Category {
+  _id: string;
+  image: string;
+  name: string;
+}
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `
+    *[_type == "category"]
+    `
+      )
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <ScrollView
       horizontal
       contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
     >
       {/* Categories Card */}
-      <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="first" />
+      {categories &&
+        categories.map((category: Category) => (
+          <CategoriesCard
+            key={category._id}
+            imgUrl={urlFor(category.image).width(200).url()}
+            title={category.name}
+          />
+        ))}
+      {/* <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
       <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
       <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
       <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
-      <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
-      <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" />
+      <CategoriesCard imgUrl="https://links.papareact.com/gn7" title="second" /> */}
     </ScrollView>
   );
 };
