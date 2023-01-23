545f77b4a4b66f2ea0476d55b53691051d43f8c5
Mohamed <mhassankd35@gmail.com>
2023-01-22 19:13:22 -0600

    creates typesafe navigation interface

--- App.tsx
+++ App.tsx
@@ -2,11 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView, Text, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
-import RestaurantScreen from "./screens/RestaurantScreen";
+import RestaurantScreen, { Dish } from "./screens/RestaurantScreen";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import BasketScreen from "./screens/BasketScreen";
-const Stack = createNativeStackNavigator();
+
+export type RootStackParams = {
+  Home: undefined;
+  Restaurant: {
+    id: string;
+    imgUrl: string;
+    title: string;
+    rating: number;
+    genre: string;
+    address: string;
+    shortDescription: string;
+    dishes: Dish[];
+    long: number;
+    lat: number;
+  };
+  Basket: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
 
 export default function App() {
   return (
