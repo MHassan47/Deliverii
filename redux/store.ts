8aba45320b6dea380d63256aa165c664b64c0a0f
Mohamed <mhassankd35@gmail.com>
2023-01-22 19:13:02 -0600

    creates state interface

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "./features/basketSlice";
+import basketReducer, { BasketSlice } from "./features/basketSlice";
+import { RestaurantSlice } from "./features/restaurantSlice";
+
+export interface State {
+  basket: BasketSlice;
+  restaurant: RestaurantSlice;
+}
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
